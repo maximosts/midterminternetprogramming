41fd5a58363b3f93c21d29db47b076ae61c4bacf
maximosts <97444382+maximosts@users.noreply.github.com>
2023-11-13 00:06:30 +0200

    Update script.js
    added sorting feature

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
 $(document).ready(function() {
-    $('#addItemForm').submit(function(e) {
-        e.preventDefault();
-        var name = $('#itemName').val();
-        var category = $('#itemCategory').val();
-        $('#itemsTable tbody').append('<tr><td>' + name + '</td><td>' + category + '</td><td><button class="btn btn-danger deleteBtn">Delete</button></td></tr>');
-        $('#itemName').val('');
-        $('#itemCategory').val('');
+    // Array to store entries
+    var entries = [];
+
+    // Function to add an entry to the table
+    function addEntry(name, category) {
+        var row = '<tr><td>' + name + '</td><td>' + category + '</td><td><button class="btn btn-danger btn-delete">Delete</button></td></tr>';
+        $('#entriesTable tbody').append(row);
+    }
+
+    // Function to refresh the table with filtered and sorted entries
+    function refreshTable(filterCategory) {
+        $('#entriesTable tbody').empty();
+        // Sort entries by category
+        entries.sort(function(a, b) {
+            return a.category.localeCompare(b.category);
+        });
+
+        $.each(entries, function(index, entry) {
+            if (filterCategory === 'all' || entry.category === filterCategory) {
+                addEntry(entry.name, entry.category);
+            }
+        });
+    }
+
+    // Add entry when the form is submitted
+    $('#addForm').submit(function(event) {
+        event.preventDefault();
+        var name = $('#entryName').val();
+        var category = $('#category').val();
+        entries.push({ name: name, category: category });
+        addEntry(name, category);
+        $('#entryName').val('');
     });
 
-    $(document).on('click', '.deleteBtn', function() {
-        $(this).closest('tr').remove();
+    // Delete entry when the delete button is clicked
+    $('#entriesTable').on('click', '.btn-delete', function() {
+        var row = $(this).closest('tr');
+        var index = row.index();
+        entries.splice(index, 1);
+        row.remove();
     });
+
+    // Sort and filter entries when the sort category is changed
+    $('#sortCategory').change(function() {
+        var filterCategory = $(this).val();
+        refreshTable(filterCategory);
+    });
+
+    // Initialize the table with all entries
+    refreshTable('all');
 });
-
-function sortTable(n) {
-    // Sorting functionality to be implemented here
-}
