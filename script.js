d328f8da0cf098e7f524e81d7b77a1fd96c22369
Your Name <a.panussis@acg.edu>
2023-11-15 09:34:04 +0200

    -Added several image assets, all of which are custom -Added buttons with scripted functionality for Alphabetical Sort by Entry name and by Category -Made thematical changes (color, margin, borders).

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ $(document).ready(function() {
                 addEntry(entry.name, entry.category);
             }
         });
+
+        
     }
 
     // Add entry when the form is submitted
@@ -32,6 +34,74 @@ $(document).ready(function() {
         addEntry(name, category);
         $('#entryName').val('');
     });
+
+
+
+
+
+    //Sort currently shown entries in table - BEGIN - Panussis
+
+    //variables (cache elements)
+    var $table = $('#entriesTable'); //getting table
+    var $tableBody = $table.find('tbody'); //getting the body of table
+    var rows, sortedRows; //creating a variables to store data of current rows and new sorted rows
+
+    //sort the table row based on the text of the first div
+    function sortEntryNameRows(a, b){
+
+        if ( $(a).find('td:first-Child').text() > $(b).find('td:first-Child').text() ) { //previous row is alphabetically superior to next row
+            return 1; 
+        }
+        if ( $(a).find('td:first-Child').text() < $(b).find('td:first-Child').text() ) { //previous row inferior to the next row
+            return -1;
+        }
+        return 0;
+    }
+
+    function sortCategoryRows(a, b){
+
+        if ( $(a).find('td:first-Child + td').text() > $(b).find('td:first-Child + td').text() ) { //previous row is alphabetically superior to next row. Adding a 'td' after 'first-Child' to check for 2nd column of tableEntries.
+            return 1; 
+        }
+        if ( $(a).find('td:first-Child + td').text() < $(b).find('td:first-Child + td').text() ) { //previous row inferior to the next row. Adding a 'td' after 'first-Child' to check for 2nd column of tableEntries.
+            return -1;
+        }
+        return 0;
+    }
+
+    $("#btnSortAZEntries").on('click', function(){ // give the 'Sort Entries A-Z' button an event for when its clicked
+
+        //get the rows from the table
+        rows = $tableBody.find('tr');
+
+        //sort the rows as "sortRows"
+        sortedRows = rows.sort(sortEntryNameRows);
+
+        //replace the old rows with the new rows
+        $tableBody.remove('tr');
+        $tableBody.append(sortedRows);
+
+    });
+
+    $("#btnSortAZCategories").on('click', function(){ // give the 'Sort Categories A-Z' button an event for when its clicked
+
+        //get the rows from the table
+        rows = $tableBody.find('tr');
+
+        //sort the rows as "sortRows"
+        sortedRows = rows.sort(sortCategoryRows);
+
+        //replace the old rows with the new rows
+        $tableBody.remove('tr');
+        $tableBody.append(sortedRows);
+
+    });
+    //Sort currently shown entries in table - END - Panussis
+
+    
+
+
+
 
     // Delete entry when the delete button is clicked
     $('#entriesTable').on('click', '.btn-delete', function() {
@@ -46,7 +116,11 @@ $(document).ready(function() {
         var filterCategory = $(this).val();
         refreshTable(filterCategory);
     });
-
+    
     // Initialize the table with all entries
     refreshTable('all');
+
 });
+
+
+ 
